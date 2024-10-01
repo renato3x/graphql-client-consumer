c2bbf168065ac5715610771f95bdb841cfb1e246
renato3x <pereirarenato21@gmail.com>
2024-09-30 21:15:49 -0300

    add: apollo client

---
+++ src/global/graphql/index.ts
@@ -0,0 +1,23 @@
+import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    }
+  }
+});
+
+const graphql = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export { graphql };
