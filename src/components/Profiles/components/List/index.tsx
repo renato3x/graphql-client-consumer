4e00ee83d6fdd57f5c2789d87c930d28dd427308
renato3x <pereirarenato21@gmail.com>
2024-10-03 00:37:14 -0300

    add: showing errors in list profiles

--- src/components/Profiles/components/List/index.tsx
+++ src/components/Profiles/components/List/index.tsx
@@ -2,12 +2,23 @@ import { useQuery } from '@apollo/client'
 import { Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
 import { Profile } from '../../../../models/profile'
 import { GET_PROFILES } from '../../../../global/graphql/queries'
+import { useState } from 'react';
+import Error from '../../../common/Error';
 
 function List() {
-  const getProfiles = useQuery<{ profiles: Profile[] }>(GET_PROFILES);
+  const getProfiles = useQuery<{ profiles: Profile[] }>(GET_PROFILES, {
+    onError(error) {
+      const serverErrors = error.graphQLErrors.map((e) => e.message);
+      setErrors(serverErrors.length > 0 ? serverErrors : ['An error occurred']);
+    },
+  });
+  const [ errors, setErrors ] = useState<string[]>([]);
 
   return (
     <Stack spacing={2}>
+      <Stack spacing={2}>
+        {errors.map((error, index) => <Error key={index} message={error}/>)}
+      </Stack>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
