68bb297899147e2fe0ed7ab7ffdc71ec73f907ea
renato3x <pereirarenato21@gmail.com>
2024-10-03 00:34:48 -0300

    add: list profiles

--- src/components/Profiles/components/List/index.tsx
+++ src/components/Profiles/components/List/index.tsx
@@ -1,11 +1,13 @@
-import { Box, Button, Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { useQuery } from '@apollo/client'
+import { Paper, Stack, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material'
+import { Profile } from '../../../../models/profile'
+import { GET_PROFILES } from '../../../../global/graphql/queries'
 
 function List() {
+  const getProfiles = useQuery<{ profiles: Profile[] }>(GET_PROFILES);
+
   return (
     <Stack spacing={2}>
-      <Box>
-        <Button variant="contained">Get Profiles</Button>
-      </Box>
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -16,7 +18,13 @@ function List() {
             </TableRow>
           </TableHead>
           <TableBody>
-            
+            {getProfiles.data?.profiles?.map((profile) => (
+              <TableRow key={profile.id}>
+                <TableCell>{ profile.id }</TableCell>
+                <TableCell>{ profile.name }</TableCell>
+                <TableCell>{ profile.label }</TableCell>
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
       </TableContainer>
